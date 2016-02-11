4858d2ab6a9b17984ab47c881667fe24f128dd7f
Hoiyen <hoiyen.2000@gmail.com>
2016-02-12 06:28:41 +0700

    Router

--- includes/Honako/Routing/Router.php
+++ includes/Honako/Routing/Router.php
@@ -1,30 +1,57 @@
 <?php
 namespace Honako\Routing;
 use \AltoRouter;
+use Closure;
 
 class Router
 {
   const VERSION = '0.0.1';
 
-  protected  $router;
+  protected $routes;
+  protected $prefix;
+  protected $namespace;
 
   public function __construct()
   {
-    $this->router = new AltoRouter;
+
   }
 
-  public function get( $route, $action, $name = null )
+  public function get( $uri, $action, $name = null )
   {
-    $this->router->map( 'GET', $route, $action, $name );
+    $this->call( 'GET', $uri, $action, $name );
   }
 
-  public function post( $route, $action, $name = null )
+  public function post( $uri, $action, $name = null )
   {
-    $this->router->map( 'POST', $route, $action, $name );
+    $this->call( 'POST', $uri, $action, $name );
+  }
+
+  public function call( $method, $uri, $action, $name = null )
+  {
+    if ( ! empty($this->prefix) )
+      $uri = $this->prefix . '/' . trim($uri, '/');
+
+    $this->routes[] = [$method, $uri, $action, $name];
+  }
+
+  public function group( $options = array(), Closure $closure )
+  {
+    if ( ! empty($options) ) {
+
+    }
+    else {
+      throw new \Exception( 'Router group parameter is empty' );
+    }
+    call_user_func( $closure, $this );
+  }
+
+  public function routes()
+  {
+    return $this->routes;
   }
 
   public function run()
   {
-    return $this->router->match();
+
   }
 }
