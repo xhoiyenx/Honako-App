d4290536e171e9fd2fe9fd0d08d6d265a5ba47f7
xhoiyenx <hoiyen.2000@gmail.com>
2016-02-12 18:35:42 +0700

    Router Group

--- includes/Honako/Routing/Router.php
+++ includes/Honako/Routing/Router.php
@@ -1,30 +1,93 @@
 <?php
 namespace Honako\Routing;
 use \AltoRouter;
+use Closure;
 
 class Router
 {
   const VERSION = '0.0.1';
 
-  protected  $router;
+  protected $routes;
+  protected $prefix;
+  protected $namespace;
+  protected $is_group = false;
 
   public function __construct()
   {
-    $this->router = new AltoRouter;
+
   }
 
-  public function get( $route, $action, $name = null )
+  public function get( $uri, $action, $name = null )
   {
-    $this->router->map( 'GET', $route, $action, $name );
+    $this->call( 'GET', $uri, $action, $name );
   }
 
-  public function post( $route, $action, $name = null )
+  public function post( $uri, $action, $name = null )
   {
-    $this->router->map( 'POST', $route, $action, $name );
+    $this->call( 'POST', $uri, $action, $name );
+  }
+
+  public function call( $method, $uri, $action, $name = null )
+  {
+    if ( ! empty($this->prefix) )
+      $uri = trim($this->prefix, '/') . '/' . trim($uri, '/');
+
+    if ( ! empty($this->namespace) )
+      $action = trim($this->namespace, '\\') . '\\' . $action;
+
+    if ( $uri != '/')
+      $uri = trim($uri, '/');
+
+    $this->routes[] = [$method, $uri, $action, $name];
+  }
+
+  public function group( $options = array(), Closure $closure )
+  {
+    if ( ! empty($options) ) {
+
+      if ( $this->is_group ) {
+        if ( ! empty($options['prefix']) ) {
+          $this->prefix .= $options['prefix'] . '/';
+        }
+
+        if ( ! empty($options['namespace'])) {
+          $this->namespace .= $options['namespace'] . '\\';
+        }
+      }
+      else {
+        if ( ! empty($options['prefix']) ) {
+          $this->prefix = $options['prefix'] . '/';
+        }
+        else {
+          $this->prefix = null;
+        }        
+
+        if ( ! empty($options['namespace'])) {
+          $this->namespace = $options['namespace'] . '\\';
+        }
+        else {
+          $this->namespace = null;
+        }
+      }
+
+      $this->is_group = true;
+    }
+    else {
+      throw new \Exception( 'Router group parameter is empty' );
+    }
+
+    call_user_func( $closure, $this );
+
+    $this->is_group = false;
+  }
+
+  public function routes()
+  {
+    return $this->routes;
   }
 
   public function run()
   {
-    return $this->router->match();
+
   }
 }
